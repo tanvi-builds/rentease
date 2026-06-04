583aa814b33a0fb86de2f8f64e32715976cdc855
tanvi-builds <tanvisonar2611gmail.com>
2026-06-04 14:08:25 +0530

    Connected frontend to backend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -15,7 +15,7 @@ function Login() {
     }
     setLoading(true);
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://rentease-0pao.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
